137941434f47fe2a1b2591a091610557ffb4270c
quatre29 <quatre29@gmail.com>
2022-09-30 10:35:23 +0100

    dockerzied

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,2 +1,2 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:9090" : "";
+export const BASE_URL = "/api";
+// process.env.NODE_ENV === "development" ? "http://localhost:9090" : "";
